5544f10b1d2bd74942d807c61e9c88bc80948bca
Cristina Bors <bors_cristina2007@yahoo.co.uk>
2025-03-05 14:04:08 +0200

    new folder

---
+++ JS/JS1/age-calculator.js
@@ -0,0 +1,8 @@
+/*function calculateEarnings(hoursWorked) {
+    const hourlyRate = 10; // $10 per hour
+    const earnings = hoursWorked * hourlyRate;
+    return earnings;}*/
+
+
+const hourlyRate = 10; // $10 per hour
+const earnings = hoursWorked * hourlyRate;
