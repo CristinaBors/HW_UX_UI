0c6f9d96aa2f68fa512da42b8d507dc8427e9bc5
Cristina Bors <bors_cristina2007@yahoo.co.uk>
2025-04-14 23:32:26 +0300

    brand

---
+++ JS/L41/brand.js
@@ -0,0 +1,11 @@
+companyName = 'CODEX' 
+companyLegalName = 'CODEX ltd.'
+companyContactPhone = '+1234567'
+companySlogan = 'Coding with eXprience'
+
+alert (
+'brand:           ' + companyName + "\n" +
+'legal name:   ' + companyLegalName + "\n" +
+'phone:          ' + companyContactPhone + "\n\n" +
+'slogan:           ' + companySlogan
+)
